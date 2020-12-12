1f386f6fbba74a06e6e7e49d78f724dc61133673
Your Name <you@example.com>
2020-12-12 13:08:12 +0300

    Konec_21

--- src/Components/Dialogs/Dialogs.js
+++ src/Components/Dialogs/Dialogs.js
@@ -3,8 +3,20 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialog
+        <div className={s.Dialogs}>
+            <div className={s.DialogsItem}>
+                <div className={s.Dialog + ' ' + s.activeItem}>Саня</div>
+                <div className={s.Dialog}>Полина</div>
+                <div className={s.Dialog}>Андрей</div>
+                <div className={s.Dialog}>Никита</div>
+                <div className={s.Dialog}>Олеся</div>
+                <div className={s.Dialog}>Алиса</div>
+            </div>
+            <div className={s.Messages}>
+                <div className={s.Message}>Привет</div>
+                <div className={s.Message}>Ну здарова</div>
+                <div className={s.Message}>Здоровей видали</div>
+            </div>
         </div>
     )
 }
