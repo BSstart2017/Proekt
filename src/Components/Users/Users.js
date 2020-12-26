f7f8f673370d2018ae928131f4c6b575cba0c020
Your Name <you@example.com>
2020-12-26 11:51:47 +0300

    :

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -3,35 +3,44 @@ import s from './Users.module.css';
 import * as axios from 'axios';
 import PhotoAvaters from './../../assets/img/photoAvatar.png';
 
-const Users = (props) => {
-    if (props.users.length === 0){
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsersAC (response.data.items)
+            this.props.setUsersAC(response.data.items)
         });
-
     }
-    return <div>
-        {props.users.map(u => <div key={u.id}>
+
+    render() {
+        return <div>
+            {this.props.users.map(u => <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photos.small != null ? u.photos.small : PhotoAvaters} className={s.userPhoto} alt='noPhoto'/>
+                    <img src={u.photos.small != null ? u.photos.small : PhotoAvaters} className={s.userPhoto}
+                         alt='noPhoto'/>
                 </div>
                 <div>
                     {u.fallowed
-                        ? <button onClick={() => {props.unfallowAC(u.id)}}>Удалить</button>
-                        : <button onClick={() => {props.fallowAC(u.id)}}>Добавить</button>}
+                        ? <button onClick={() => {
+                            this.props.unfallowAC(u.id)
+                        }}>Удалить</button>
+                        : <button onClick={() => {
+                            this.props.fallowAC(u.id)
+                        }}>Добавить</button>}
                 </div>
             </span>
-            <span>
+                <span>
                  <div>{u.name}</div>
                 <div>{u.status}</div>
             </span>
-            <span>
+                <span>
                  <div>{"u.location.country"}</div>
                 <div>{"u.location.city"}</div>
             </span>
-        </div>)}
-    </div>
+            </div>)
+            }
+        </div>
+    }
 }
 
 export default Users;
